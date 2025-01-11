c710568f3255f09f05da81ce427a12a9a0b04d54
kartikeySingh2077 <kartikeysinghshikai@gmail.com>
2025-01-11 09:54:22 +0530

    Create Vectors-II.r

---
+++ Vectors-II.r
@@ -0,0 +1,8 @@
+# A numeric vector containing 3 elements
+numeric_vector <- c(1, 10, 49)
+
+# a character vector containing 3 elements
+character_vector <- c("a", "b", "c")
+
+# Complete the code for 'boolean_vector'
+boolean_vector <- c(TRUE, FALSE, TRUE)
