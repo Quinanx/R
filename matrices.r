dbb362d4603e0d76f1ed50ef42667dc3528aa6cb
kartikeySingh2077 <kartikeysinghshikai@gmail.com>
2025-01-11 10:14:29 +0530

    Create matrices.r
    You can construct a matrix in R with the matrix() function. Consider the following example: matrix(1:9, byrow = TRUE, nrow = 3, ncol = 3)

---
+++ matrices.r
@@ -0,0 +1,5 @@
+# Construction of a matrix with 5 rows that contain the numbers 1 up to 20 and assign it to m
+m = matrix(1:20 , byrow = TRUE , nrow = 5 , ncol = 4)
+
+# print m to the console
+print(m)
