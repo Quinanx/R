6e2e5bd346535bf26a5f4fc2608b8832338a25d0
kartikeySingh2077 <kartikeysinghshikai@gmail.com>
2025-01-11 08:35:47 +0530

    Create Variable-assignment-II.r
    Variables are great to perform arithmetic operations with. In this assignment, we have defined a variable my_apples. You want to define another variable called my_oranges and add these two together.

---
+++ Variable-assignment-II.r
@@ -0,0 +1,9 @@
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+
+my_oranges <- 6
+# Make sure to print the result right after adding the variables
+print(my_apples + my_oranges)
+
+# Ensure the variable name for the total is correct
+my_fruit <- my_apples + my_oranges
