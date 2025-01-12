baadaa50b1d83950d46d6bc8d2fb15cb0d552f9e
Kartikey Singh <kartikeysinghshikai@gmail.com>
2025-01-12 23:53:45 +0530

    Create Functions-continued.r

---
+++ Functions-continued.r
@@ -0,0 +1,8 @@
+# a grades vector
+grades <- c(8.5, 7, 9, 5.5, 6)
+
+# calculate the mean of grades using matching by name
+mean (x = grades)
+
+# calculate the mean of grades using matching by position
+mean(grades)
